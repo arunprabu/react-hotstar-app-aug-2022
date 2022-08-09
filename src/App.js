6b3bb23e1730b041cdb0e3408f623ba7a857707f
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-08-09 19:39:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 // Ideal comp for your layout
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
+import Concepts from './components/Concepts/Concepts';
+import About from './components/About/About';
 
 // App Component 
 function App() {
   // must return JSX 
   return (
-    
-    <div>
-      <Header />
+    <BrowserRouter>
+      <div>
+        <Header />
 
-      <div className='container mt-5'>
-        <Home />
+        <div className='container mt-5'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/concepts' element={<Concepts />} />
+            <Route path='/about' element={<About />} />
+            {/* TODO: learn about 404 */}
+          </Routes>
+        </div>
+
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,19 @@
 // Functional Component with Arrow Function
 
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
   return(
     <ul className="navbar-nav me-auto mb-2 mb-md-0">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="/">Home</a>
+        <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">Concepts</a>
+        <NavLink className="nav-link" to="/concepts">Concepts</NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">About</a>
+        <NavLink className="nav-link" to="/about">About</NavLink>
       </li>
     </ul>
   )
